f985b7a85699ab124d89b4775976ce5d0ff31f3d
cameronchiu <mohonkCC01>
2019-10-18 12:00:33 -0500

    Pseuodocode within Main Activity

--- app/src/main/java/com/example/growyourhealth/MainActivity.java
+++ app/src/main/java/com/example/growyourhealth/MainActivity.java
@@ -15,29 +15,144 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    DateFormat dateFormat = new SimpleDateFormat("d");
+    DateFormat monthFormat = new SimpleDateFormat("MM");
+    private int currentDayOfYear;
+    private boolean weekInProgress;
+
+    //Preferences
+    private int stepsGoal;
+    private int cupsOfWaterGoal;
+    private int numberOfMealsGoal;
+    private int caloriesConsumedGoal;
+    private int hoursOfSleepGoal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Sets home screen button to Sunday, Monday, Tuesday, etc.
         DateFormat dayFormat = new SimpleDateFormat("EEEE");
         String day = dayFormat.format(Calendar.getInstance().getTime());
         Button startDay = (Button) findViewById(R.id.buttonDayOfWeek);
         startDay.setText(day);
 
+        //gets current day (out of 365)
+        String currentMonthString = monthFormat.format(Calendar.getInstance().getTime());
+        int currentMonthInt = Integer.valueOf(currentMonthString);
+        String currentDayOfMonthString = dateFormat.format(Calendar.getInstance().getTime());
+        int currentDayOfMonthInt = Integer.valueOf(currentDayOfMonthString);
+        if(currentMonthInt==1){
+            currentDayOfYear = currentDayOfMonthInt;
+        }
+        if(currentMonthInt==2){
+            currentDayOfYear = 31+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==3){
+            currentDayOfYear = 60+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==4){
+            currentDayOfYear = 90+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==5){
+            currentDayOfYear = 121+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==6){
+            currentDayOfYear = 151+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==7){
+            currentDayOfYear = 182+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==8){
+            currentDayOfYear = 212+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==9){
+            currentDayOfYear = 243+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==10){
+            currentDayOfYear = 273+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==11){
+            currentDayOfYear = 304+currentDayOfMonthInt;
+        }
+        if(currentMonthInt==12){
+            currentDayOfYear = 334+currentDayOfMonthInt;
+        }
+
+        //updates tree image
+        if(lastDayScore >= 45){
+            //grow two stages
+        }
+        if(lastDayScore >=30&&lastDayScore<45){
+            //grow one stage
+        }
+        else{
+            //doesn't grow any stages
+        }
+
+
 
     }
 
-    DateFormat dateFormat = new SimpleDateFormat("d");
-    String date = dateFormat.format(Calendar.getInstance().getTime());
-    private int startDate = 0; //variable extracted from database
-    private int currentDate = Integer.valueOf(date);
-
-    public void updateTree(View V){
-        TextView test = (TextView) findViewById(R.id.textViewtest);
 
 
 
-        test.setText(date);
+
+    public void growTree(View V){
+        //button at end of questions activity
+        //extracts user input and calculates score. Then inputs it into database (lastDayScore)
+        //if beginning of week, set startDate in database
+        int todayScore = 0;
+        int inputSteps;
+        int inputCupsOfWater;
+        int inputMealsEaten;
+        int inputCaloriesConsumed;
+        int inputHoursOfSleep;
+
+        //extract values from plain text boxes and assign to variables above
+
+        if(inputSteps>=stepsGoal&&inputSteps<stepsGoal*1.5){
+            todayScore += 5;
+        }
+        if(inputSteps>stepsGoal*1.5){
+            todayScore += 10;
+        }
+        if(inputCupsOfWater>=cupsOfWaterGoal&&inputCupsOfWater<cupsOfWaterGoal*1.5){
+            todayScore += 5;
+        }
+        if(inputCupsOfWater>cupsOfWaterGoal*1.5){
+            todayScore += 10;
+        }
+        if(inputMealsEaten>=numberOfMealsGoal&&inputMealsEaten<numberOfMealsGoal*1.5){
+            todayScore += 5;
+        }
+        if(inputMealsEaten>numberOfMealsGoal*1.5){
+            todayScore += 10;
+        }
+        if(inputCaloriesConsumed>=caloriesConsumedGoal&&inputSteps<caloriesConsumedGoal*1.5){
+            todayScore += 5;
+        }
+        if(inputCaloriesConsumed>caloriesConsumedGoal*1.5){
+            todayScore += 10;
+        }
+        if(inputHoursOfSleep>=hoursOfSleepGoal&&inputHoursOfSleep<hoursOfSleepGoal*1.5){
+            todayScore += 5;
+        }
+        if(inputHoursOfSleep>hoursOfSleepGoal*1.5){
+            todayScore += 10;
+        }
+
+        if(!weekInProgress){
+            //updates lastDayScore
+            //lastDayScore = todayScore
+            //sets startDate
+        }
+        else{
+            //updates lastDayScore
+            //lastDayScore = todayScore
+        }
+
+        //updates database with lastDayScore
 
     }
 
@@ -45,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
         //sends user to questions activity
         //sets button to either "Grow Tree' or "Plant Tree"
         Button submit = (Button) findViewById(R.id.buttonSubmit);
-        if(startDate-currentDate==0){
+        if(startDate==currentDayOfYear){
             submit.setText("Plant Tree");
         }
         else{
@@ -53,4 +168,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
 }
---
+++ app/src/main/java/com/example/growyourhealth/ScoreSaver.java
@@ -0,0 +1,4 @@
+package com.example.growyourhealth;
+
+public class ScoreSaver {
+}
